539b1020664b97410d2a78a0881e67133e0215bc
Rohan Liston <rohanliston@users.noreply.github.com>
2016-06-04 23:56:11 +1000

    Add Sheetifier class to generate the actual spritesheet

---
+++ after_effects_spritesheet_exporter/(support)/Sheetifier.jsx
@@ -0,0 +1,65 @@
+/**
+ * Sheetifier: Generates sprite sheets from After Effects compositions.
+ */
+function Sheetifier(sourceComp, config)
+{
+    this.config = config;
+    this.sourceComp = sourceComp;
+    this.destComp = null;
+
+    /**
+     * Creates a sprite sheet according to the supplied configuration and exports the desired output files.
+     */
+    this.sheetify = function()
+    {
+        var currentFrame = 1;
+        var frameWidth = this.config["frameSize"]["width"];
+        var frameHeight = this.config["frameSize"]["height"];
+
+        // Create destination composition.
+        this.createDestComp();
+        this.destComp.openInViewer();
+
+        // Import the source composition once per frame and shift its start time back one frame.
+        for(var row = this.config["numFrames"]["rows"]-1; row >= 0; --row)
+        {
+            for(var col = this.config["numFrames"]["cols"]-1; col >= 0; --col)
+            {
+                var frame = this.destComp.layers.add(this.sourceComp);
+
+                // Move frame into position.
+                frame.property("Transform").property("Anchor Point").setValue([0,0]);
+                frame.property("Transform").property("Position").setValue([col * frameWidth, row * frameHeight]);
+
+                // Shift start time so all sub-frames appear at destComp's first frame.
+                frame.startTime = this.timeAtFrame(currentFrame) - this.timeAtFrame(this.config["numFrames"]["total"]);
+
+                ++currentFrame;
+            }
+        }
+    };
+
+    /**
+     * Creates the composition to which the sprite sheet will be drawn.
+     * Settings such as pixel aspect and frame rate will match those of the source composition.
+     */
+    this.createDestComp = function()
+    {
+        this.destComp = app.project.items.addComp(
+            config["sheetName"] + "_spritesheet",
+            config["sheetPixelSize"]["width"],
+            config["sheetPixelSize"]["height"],
+            this.sourceComp.pixelAspect,
+            this.timeAtFrame(1),
+            this.sourceComp.frameRate
+        );
+    }
+
+    /**
+     * Returns the time of the given frame in seconds.
+     */
+    this.timeAtFrame = function(frame)
+    {
+        return frame / this.sourceComp.frameRate;
+    }
+}
